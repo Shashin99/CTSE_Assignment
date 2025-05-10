c540e6e5ec9b0bc6a22b7eb5f6fe29b687d55fa6
K L Shashin Kalpajith <shashikalpajith6@gmail.com>
2025-05-10 10:39:10 +0530

    Add separated CI/CD pipelines for microservices

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,8 +1,8 @@
 const config = {
-    AUTH_SERVICE_URL: "http://localhost:5001",
-    USER_SERVICE_URL: "http://localhost:5002",
-    PRODUCT_SERVICE_URL: "http://localhost:5004",
-    CART_SERVICE_URL: "http://localhost:5003",
+    AUTH_SERVICE_URL: "http://16.171.152.49:5001",
+    USER_SERVICE_URL: "http://51.20.105.160:5002",
+    PRODUCT_SERVICE_URL: "http://51.20.134.127:5004",
+    CART_SERVICE_URL: "http://51.21.244.2:5003",
 };
 
 export default config;
